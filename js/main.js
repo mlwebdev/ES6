2d279ce5e17a87a447ebdb4dfe28ffc723ce29d9
mlwebdev <mlwebdeveloper@hotmail.com>
2017-12-01 18:22:51 -0500

    Iterable object

--- js/main.js
+++ js/main.js
@@ -39,3 +39,37 @@ var it = iter(['Hello', 'Iterators']);
 console.log(it.next().value); // 'Hello'
 console.log(it.next().value); // 'Iterators'
 console.log(it.next().done);  // true
+
+
+
+//An iterable object    
+//1. Has a method with key has 'Symbol.iterator'    
+//2. This method returns an iterator via method 'next'    
+
+let iter = {
+  0: 'Hello',
+  1: 'World of ',
+  2: 'Iterators',
+  length: 3,
+  [Symbol.iterator]() {
+    let index = 0;
+    return {
+      next: () => {
+        let value = this[index];
+        let done = index >= this.length;
+        index++;
+        return {
+          value,
+          done
+        };
+      }
+    };
+  }
+};
+for (let i of iter) {
+  console.log(i);
+}
+// Prints 
+"Hello"
+"World of "
+"Iterators"
