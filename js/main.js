52234c310e15322d237c9e7ab7018fff60094bf0
mlwebdev <mlwebdeveloper@hotmail.com>
2017-12-01 17:17:12 -0500

    Add for of loop

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// ES6 for of loop replaces both the for...in and for...each loop 
+const iter = [	
+	'a', 'b'
+];    
+
+for (const i of iter) {      
+	console.log(i);    
+} /* prints 
+	 * "a"    
+	 * "b"
+	*/
